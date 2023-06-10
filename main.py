1b83ddebf1dfbefe35461cca4ade533bae9ec9d0
Daniel Košč <daniel.kosc@gmail.com>
2023-06-10 22:52:29 +0200

    Data augmentation fully implemented for whole directory

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import tensorflow as tf
 from keras_preprocessing.image import ImageDataGenerator, load_img, img_to_array
+from os import listdir
+from os.path import isfile, join
 
-train_data_generator = ImageDataGenerator(rotation_range=15,
-                                   width_shift_range=0.15,
-                                   height_shift_range=0.15,
-                                   shear_range=0.2,
-                                   zoom_range=0.2,
-                                   horizontal_flip=True,
-                                   fill_mode='reflect')
 
-pic = load_img('test_data/520.jpg')
-pic_array = img_to_array(pic)
-pic_array = pic_array.reshape((1,) + pic_array.shape)
+def augment_pictures(multiplier, path):
+    train_data_generator = ImageDataGenerator(rotation_range=15,
+                                              width_shift_range=0.15,
+                                              height_shift_range=0.15,
+                                              shear_range=0.2,
+                                              zoom_range=0.2,
+                                              horizontal_flip=True,
+                                              fill_mode='reflect')
+    pictures = [f for f in listdir(path) if isfile(join(path, f)) and f.split(".")[-1] == 'jpg']
+    for picture in pictures:
+        pic = load_img(path + '/' + picture)
+        pic_array = img_to_array(pic)
+        pic_array = pic_array.reshape((1,) + pic_array.shape)
 
-count = 0
-for batch in train_data_generator.flow(pic_array, batch_size=1, save_to_dir='test_data/generated_images', save_prefix='generated', save_format='jpeg'):
-    count += 1
-    if count == 4:
-        break
+        count = 0
+        for batch in train_data_generator.flow(pic_array, batch_size=1, save_to_dir=path + '/generated_images',
+                                               save_prefix='generated', save_format='jpeg'):
+            count += 1
+            if count == multiplier:
+                break
 
+
+
+augment_pictures(4, 'test_data')
