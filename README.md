ce553fca8d5468fe4446376cc096000f7167d5ac
Daniel Košč <daniel.kosc@gmail.com>
2023-06-10 17:23:48 +0200

    Data augmentation

---
+++ README.md
@@ -0,0 +1,21 @@
+# Main dataset
+- Kaggle https://www.kaggle.com/datasets/meliodassourav/traffic-violation-dataset-v3?resource=download
+- Many problem:
+  - duplicate images
+  - incorrect labels
+  - drawing or computer animation instead of pictures
+  - watermarks
+  - not uniform size/quality
+  - blured
+  - small
+    
+
+# Data augmentation
+- using ImageDataGenerator from keras_preprocessing.image
+  - increase number of training images by:
+    - rotation by up to 15 degrees (real photo conditions)
+    - zoom in up to 20%
+    - mirror effect (horizontal flip)
+    - add missing pixel when rotation or moving by reflecting existing in the original picture
+    - move horizontally and vertically up to 15 %
+    - shear transformation up to 20% https://en.wikipedia.org/wiki/Shear_mapping
--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import tensorflow as tf
+from keras_preprocessing.image import ImageDataGenerator
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+train_datagen = ImageDataGenerator(rotation_range=15,
+                                   width_shift_range=0.15,
+                                   height_shift_range=0.15,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   fill_mode='reflect')
