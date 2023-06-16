4686f91febc6c4e122ff39b24f2f9643f5790d75
Daniel Košč <daniel.kosc@gmail.com>
2023-06-16 16:03:32 +0200

    Full model with bad accuracy

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from keras_preprocessing.image import ImageDataGenerator, load_img, img_to_array
 from os import listdir
 from os.path import isfile, join
 
-batch_size = 20
+batch_size = 10
 def augment_pictures(multiplier, path):
     train_data_generator = ImageDataGenerator(rotation_range=35,
                                               width_shift_range=0.15,
@@ -19,7 +19,7 @@ def augment_pictures(multiplier, path):
         pic_array = pic_array.reshape((1,) + pic_array.shape)
 
         count = 0
-        for batch in train_data_generator.flow(pic_array, batch_size=1, save_to_dir=path + '/generated_images',
+        for batch in train_data_generator.flow(pic_array, batch_size=batch_size, save_to_dir=path + '/generated_images',
                                                save_prefix='generated', save_format='jpg'):
             count += 1
             if count == multiplier:
@@ -29,7 +29,7 @@ def augment_pictures(multiplier, path):
 def get_model():
     model = tf.keras.models.Sequential([
         # 1st conv
-        tf.keras.layers.Conv2D(64, kernel_size=11, strides=8, activation='relu', input_shape=(128, 128, 3)),
+        tf.keras.layers.Conv2D(96, kernel_size=11, strides=8, activation='relu', input_shape=(128, 128, 3)),
         tf.keras.layers.BatchNormalization(),
         tf.keras.layers.MaxPooling2D(2, strides=2),
         # 2nd conv
@@ -63,7 +63,7 @@ def get_model():
 def compile_model(model):
     from keras.optimizers.legacy import Adam
     model.compile(
-        optimizer=Adam(learning_rate=0.005),
+        optimizer=Adam(learning_rate=0.001),
         loss='binary_crossentropy',
         metrics=['accuracy']
     )
@@ -76,6 +76,7 @@ def fit_model(model, train_generator, validation_generator):
                      validation_data=validation_generator,
                      validation_steps=validation_generator.samples // batch_size,
                      epochs=10)
+    return hist
 
 
 augment_pictures(4, 'data_copy/nohelmet_b')
@@ -85,7 +86,7 @@ datagen = ImageDataGenerator(rescale=1. / 255, validation_split=0.2)
 train_generator = datagen.flow_from_directory(
     directory='/Users/danielkosc/Documents/MUNI/Spring2023/ML/project/data_copy/Final_data',
     target_size=(128, 128),
-    batch_size=20,
+    batch_size=batch_size,
     class_mode='binary',
     shuffle=True,
     subset='training'
@@ -93,9 +94,43 @@ train_generator = datagen.flow_from_directory(
 validation_generator = datagen.flow_from_directory(
     directory='/Users/danielkosc/Documents/MUNI/Spring2023/ML/project/data_copy/Final_data',
     target_size=(128, 128),
-    batch_size=20,
+    batch_size=batch_size,
     class_mode='binary',
     shuffle=True,
     subset='validation'
 )
-fit_model(model, train_generator, validation_generator)
+hist = fit_model(model, train_generator, validation_generator)
+
+import matplotlib.pyplot as plt
+acc = hist.history['accuracy']
+val_acc = hist.history['val_accuracy']
+loss = hist.history['loss']
+val_loss = hist.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'r', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend(loc=0)
+plt.figure()
+plt.show()
+
+
+import numpy as np
+
+from keras.preprocessing import image
+# predicting images
+path = "/Users/danielkosc/Documents/MUNI/Spring2023/ML/project/data_copy/Final_data/nohelmet_c/generated_0_52.jpg"
+img = image.load_img(path, target_size=(128, 128))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+
+images = np.vstack([x])
+classes = model.predict(images, batch_size=1)
+print(classes[0])
+if classes[0]>0.5:
+    print("is with helmet")
+else:
+    print(" is without helmet")
+plt.imshow(img)
